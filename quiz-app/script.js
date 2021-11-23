4d02896d950eb2bce3da0e9d25778a2ea2c6a09c
Alemnew Marie <alemnewmarie461@gmail.com>
2021-11-23 18:28:31 +0300

    Add django multiple choice quizes

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -1,38 +1,48 @@
 // the list of question and their answers
+
 let quizData = [
     {
-        "question": "What is question 1?",
-        "ch1": "Answer 1",
-        "ch2": "Answer 2",
-        "ch3": "Answer 3",
-        "ch4": "Answer 4",
-        "correct": "ch3"
-    },
-    {
-        "question": "What is question 20000?",
-        "ch1": "Answer 1tt",
-        "ch2": "Answer tt2",
-        "ch3": "Answer 3tt",
-        "ch4": "Answer 4tt",
-        "correct": "ch2"
-    },
-    {
-        "question": "What is question 1?",
-        "ch1": "Answer 1",
-        "ch2": "Answer 2",
-        "ch3": "Answer 3",
-        "ch4": "Answer 4",
+        "question": "What are the features available in Django web framework?",
+        "ch1": "Admin Interface (CRUD)",
+        "ch2": "Templating",
+        "ch3": "Form handling",
+        "ch4": "All of the above",
         "correct": "ch4"
     },
     {
-        "question": "What is question 20000?",
-        "ch1": "Answer 1tt",
-        "ch2": "Answer tt2",
-        "ch3": "Answer 3tt",
-        "ch4": "Answer 4tt",
+        "question": "Which of the following is the inheritance style in Django?",
+        "ch1": "Abstract base classes",
+        "ch2": "Multi-table inheritance",
+        "ch3": "Proxy models",
+        "ch4": "All of the above",
+        "correct": "ch4"
+    },
+    {
+        "question": "Which method is used rather than a path() in urls.py to pass in regular expressions as routes?",
+        "ch1": "url()",
+        "ch2": "static()",
+        "ch3": "include()",
+        "ch4": "reg()",
+        "correct": "ch1"
+    },
+    {
+        "question": "What is the meaning of {{name}} in Django Templates?",
+        "ch1": "It will show some static value",
+        "ch2": "It would be showed as the name in HTML.",
+        "ch3": "The name will be replaced by values of Python variable.",
+        "ch4": "None",
+        "correct": "ch3"
+    },
+    {
+        "question": "What is the Django command to view a database schema of an existing (or legacy) database?",
+        "ch1": "manage.py inspect",
+        "ch2": "manage.py legacydb",
+        "ch3": "manage.py inspectdb",
+        "ch4": "None",
         "correct": "ch1"
     }
 ]
+
 
 function loadQuiz(questionNumber) {
     deselectChoices();
@@ -50,24 +60,6 @@ function loadQuiz(questionNumber) {
     ch3.innerHTML = currentQuestion["ch3"];
     ch4.innerHTML = currentQuestion["ch4"];
 }
-/*
-function getNextQuestion() {
-    if (questionNumber < quizData.length - 1) {
-        questionNumber += 1;
-        loadQuiz(questionNumber);
-    } else {
-        nextButton = document.getElementById("nextBtn");
-        nextButton.innerHTML = "Submit";
-    }
-}
-
-function getPreviousQuestion() {
-    if (questionNumber > 0) {
-        questionNumber -= 1;
-        loadQuiz(questionNumber);
-    }
-}
-*/
 
 function getSelectedChoice() {
     let answer;
@@ -91,7 +83,7 @@ function showProgress(questionNumber) {
     let progress = document.getElementById("progress");
     let progressLabel = document.getElementById("progress_label");
 
-    width = (questionNumber / quizData.length) * 100;
+    let width = (questionNumber / quizData.length) * 100;
     progress.style.width = width + "%";
     progressLabel.innerHTML = `${questionNumber}/${quizData.length}`;
 }
@@ -117,7 +109,7 @@ submitBtn.addEventListener("click", () => {
             showProgress(questionNumber + 1);
             submitBtn.innerHTML = (questionNumber == quizData.length - 1) ? "Submit" : "Next";
         } else {
-            quiz = document.getElementById("quiz");
+            let quiz = document.getElementById("quiz");
             quiz.innerHTML = `<h2>Your score: ${score}/${quizData.length}</h2>`
             submitBtn.setAttribute("onclick", "location.reload()");
             submitBtn.innerHTML = "Retake";
