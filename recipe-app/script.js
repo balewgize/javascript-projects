16bd7e72162f11997242c590693335f9f39ab016
Alemnew Marie <alemnewmarie461@gmail.com>
2021-11-25 08:12:31 +0300

    Search meals by search term

--- recipe-app/script.js
+++ recipe-app/script.js
@@ -1,6 +1,11 @@
 
 const meals = document.getElementById("meals");
 const favMeals = document.getElementById("fav-meals");
+const searchInput = document.getElementById("search-term");
+const searchBtn = document.getElementById("search"); 
+const closePopup = document.getElementById("close-popup");
+const mealPopup = document.getElementById("meal-popup");
+const mealInfo = document.getElementById("meal-info");
 
 async function getRandomMeal() {
     const response = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
@@ -17,7 +22,9 @@ async function getMealById(id) {
 }
 
 async function getMealsBySearch(term) {
-    const meals = await fetch('www.themealdb.com/api/json/v1/1/search.php?s=' + term);
+    const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' + term);
+    const responseData = await response.json();
+    return responseData.meals;
 }
 
 function saveMealToLocalStorage(mealId) {
@@ -45,7 +52,7 @@ function addMeal(meal, isRandom = false) {
     <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
     <div class="meal-body">
         <h4>${meal.strMeal}</h4>
-        <button class="fav-btn" onclick="addToFavorites()">
+        <button class="fav-btn">
             <i class="fas fa-heart"></i>
         </button>
     </div>
@@ -62,7 +69,14 @@ function addMeal(meal, isRandom = false) {
             saveMealToLocalStorage(meal.idMeal);
             favBtn.classList.add("active");
         }
+        favMeals.innerHTML = ''; // clean the container
+        showFavoriteMeals();
     });
+
+    meal.addEventListener("click", () => {
+        showMealInfo(meal);
+    });
+
 }
 
 function addMealToFavorites(meal) {
@@ -73,11 +87,21 @@ function addMealToFavorites(meal) {
     <button class="clear"><i class="fas fa-window-close"></i></button>
     `;
     favMeals.appendChild(li);
+
+    const removeBtns = document.querySelectorAll(".clear");
+    removeBtns.forEach(btn => btn.addEventListener("click", () => {
+        removeMealFromLS(meal.idMeal);
+        favMeals.innerHTML = ''; // clean the container
+        showFavoriteMeals();
+    }));
+    
+    meal.addEventListener("click", () => {
+        showMealInfo(meal);
+    });
 }
 
 async function showFavoriteMeals() {
     const favMealIds = getMealsFromLocaStorage();
-
     for (let i = 0; i < favMealIds.length; i++) {
         const mealId = favMealIds[i];
         const meal = await getMealById(mealId);
@@ -85,6 +109,48 @@ async function showFavoriteMeals() {
         addMealToFavorites(meal);
     }
 }
+
+searchBtn.addEventListener("click", async () => {
+    const term = searchInput.value;
+    const meals = await getMealsBySearch(term);
+    if (meals) {
+        meals.innerHTML = '';
+        meals.forEach(meal => {
+            addMeal(meal);
+        });
+    }
+});
+
+function showMealInfo(meal) {
+    mealInfo.innerHTML = "";
+
+    const div = document.createElement("div");
+    const ingredients = [];
+    for(let i=1; i <= 20; i++) {
+        if(meal["strIngredient" + i]) {
+            ingredients.push(`${meal["strIngredient" + i]} - ${meal["strMeasure" + i]}`);
+        } else {
+            break;
+        }
+    }
+    div.innerHTML = `
+        <h1>${meal.strMeal}</h1>
+        <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
+        <p>${meal.strInstructions}</p>
+        <h3>Ingredients:</h3>
+        <ul>
+            ${ingredients.map((ing) => `<li>${ing}</li>`).join("")}
+        </ul>
+    `;
+
+    mealInfo.appendChild(div);
+    mealPopup.classList.remove("hidden");
+}
+
+closePopup.addEventListener("click", () => {
+    mealPopup.classList.add("hidden");
+});
+
 
 getRandomMeal();
 showFavoriteMeals();
